f98c9aed8b5155b40498e1b1a5bad0ffddc08e8f
loveyChauhan <loveychauhan68363@gmail.com>
2025-06-14 05:25:11 +0530

    responsive fixed

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = ({
 }) => {
   return (
     <footer
-      className="fixed bottom-0 flex w-full items-center justify-between rounded-tl-[10px] rounded-tr-[10px] bg-[var(--color-light-gray)] px-3 py-0.5 shadow-2xl md:hidden"
+      className="fixed bottom-0 flex w-full items-center justify-between rounded-tl-[10px] rounded-tr-[10px] bg-[var(--color-light-gray)] px-4 py-0.5 shadow-2xl md:hidden"
       style={{ backgroundColor: isDark ? "#2c2c2e" : "#e0e0e0" }}
     >
       <FavoriteButton favCollection={favCollection} />
